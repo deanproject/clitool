dc8c9d5d2d919c4ec440ff537356652e8e660897
deanproject <deanmedianetwork@gmail.com>
2026-09-03 02:29:01 +0530

    url and response testing [ no concurrency ]

--- healthcheck.go
+++ healthcheck.go
@@ -7,15 +7,20 @@ import (
 
 func urlhealth() {
 
-	url := "https://www.fpwop.com/feed/"
+	//url := "https://www.fpwop.com/feed/"
 
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error", err.Error())
-		return
+	url := [4]string{"https://linkedin.com", "https://google.com", "https://fb.com", "https://fwppe.com"}
+
+	for _, urlresponse := range url {
+
+		resp, err := http.Get(urlresponse)
+		if err != nil {
+			fmt.Println("Error", err.Error())
+			continue
+		}
+
+		fmt.Printf("URL: %s | Status: %s\n", urlresponse, resp.Status)
+		resp.Body.Close()
 	}
-
-	defer resp.Body.Close()
-	fmt.Printf("Status: %d %s\n", resp.StatusCode, resp.Status)
 
 }
