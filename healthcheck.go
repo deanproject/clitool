6ba644fe245606a1de8da5f3e21ab01f662551b5
deanproject <deanmedianetwork@gmail.com>
2026-09-03 02:57:30 +0530

    Adding go routines

--- healthcheck.go
+++ healthcheck.go
@@ -3,24 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 func urlhealth() {
 
 	//url := "https://www.fpwop.com/feed/"
 
+	var wg sync.WaitGroup
+
 	url := [4]string{"https://linkedin.com", "https://google.com", "https://fb.com", "https://fwppe.com"}
 
 	for _, urlresponse := range url {
 
-		resp, err := http.Get(urlresponse)
-		if err != nil {
-			fmt.Println("Error", err.Error())
-			continue
-		}
+		wg.Add(1)
 
-		fmt.Printf("URL: %s | Status: %s\n", urlresponse, resp.Status)
-		resp.Body.Close()
+		go func(urlStr string) {
+			defer wg.Done()
+			resp, err := http.Get(urlresponse)
+			if err != nil {
+				fmt.Println("Error", err.Error())
+				return
+			}
+			defer resp.Body.Close()
+
+			fmt.Printf("URL: %s | Status: %s\n", urlStr, resp.Status)
+		}(urlresponse)
+
 	}
-
+	wg.Wait()
 }
